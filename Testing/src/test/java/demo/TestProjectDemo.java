a1009d3d745b0ee460796cb8d62b3cb0a6179c7b
Sravan <Sravan@SK>
2021-02-27 11:39:09 +0530

    First commit to git

---
+++ Testing/src/test/java/demo/TestProjectDemo.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class TestProjectDemo {
+
+	public static void main(String[] args) {
+		Test();
+	}
+	
+	public static void Test() {
+		System.out.println("This is test 1");
+	}
+
+}
